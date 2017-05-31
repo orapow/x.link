8f0fd4fc307e60eaf0a551629f4dbaff8074ca64
Jl <1633182988@qq.com>
2017-05-31 14:32:58 +0800

    文章管理

---
+++ src/Web/X.App/Views/mgr/art/edit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X.Web;
+
+namespace X.App.Views.mgr.art
+{
+    public class edit : xmg
+    {
+        public int id { get; set; }
+        protected override string GetParmNames
+        {
+            get
+            {
+                return "id";
+            }
+        }
+        protected override void InitDict()
+        {
+            base.InitDict();
+
+            if (id > 0)
+            {
+                var ent = db.x_art.FirstOrDefault(o => o.id == id);
+                if (ent == null) throw new XExcep("0x0005");
+                dict.Add("item", ent);
+            }
+
+        }
+    }
+}
