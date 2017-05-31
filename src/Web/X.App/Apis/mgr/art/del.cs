ec418422fec8b041c006bd474d472118da499b51
Jl <1633182988@qq.com>
2017-05-31 14:32:35 +0800

    文章管理

---
+++ src/Web/X.App/Apis/mgr/art/del.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using X.Core.Cache;
+using X.Web;
+using X.Web.Com;
+namespace X.App.Apis.mgr.art
+{
+    public class del : xmg
+    {
+        [ParmsAttr(min = 1)]
+        public int id { get; set; }
+
+        protected override XResp Execute()
+        {
+            var ent = db.x_art.FirstOrDefault(o => o.id == id);
+            if (ent == null) throw new XExcep("0x0008");
+
+            db.x_art.DeleteOnSubmit(ent);
+            db.SubmitDBChanges();
+
+            return new XResp();
+        }
+    }
+}
