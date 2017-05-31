ddc8eaf1908c9cce8db66008aa438a46fd8648ef
Jl <1633182988@qq.com>
2017-05-31 11:09:39 +0800

    推荐网址

---
+++ src/Web/X.App/Apis/redput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X.Web;
+using X.Web.Com;
+
+namespace X.App.Apis
+{
+    public class redput : xapi
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string con { get; set; }
+
+        protected override XResp Execute()
+        {
+            var u = db.x_user.FirstOrDefault(o => o.user_id == 1);
+            if (u == null) throw new XExcep("0x0005");
+
+            var link = new x_url();
+            link.ctime = DateTime.Now;
+            link.url = url;
+            link.title = name;
+            link.alt = con;
+            link.remark = "1";
+            link.tags = "[0]";
+            link.url_id = 1;
+
+            u.x_url.Add(link);
+            db.SubmitDBChanges();
+            return new XResp();
+        }
+
+    }
+}
