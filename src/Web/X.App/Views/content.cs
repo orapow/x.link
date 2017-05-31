9d1a7dc28817ea52fb9ba3d1b21d71f50e2ee3ca
Jl <1633182988@qq.com>
2017-05-31 16:27:26 +0800

    文章详情页

---
+++ src/Web/X.App/Views/content.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X.Web;
+
+namespace X.App.Views
+{
+    public class content : xview
+    {
+        public int id { get; set; }
+        protected override string GetParmNames
+        {
+            get
+            {
+                return "id";
+            }
+        }
+        protected override void InitDict()
+        {
+            base.InitDict();
+
+            if (id > 0)
+            {
+                var ent = db.x_art.FirstOrDefault(o => o.id == id);
+                if (ent == null) throw new XExcep("0x0005");
+                dict.Add("item", ent);
+            }
+
+        }
+    }
+}
